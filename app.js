458ede1719339149c4d26aecb3cb75d7b39de9cd
zafer.kirik <zafer.krk@hotmail.com>
2020-08-21 11:15:13 +0300

    first

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
-const Event=require('./models/event');
+const bcrypt = require('bcryptjs');
+const Event = require('./models/event');
+const User = require('./models/user');
 
 const app = express();
 const events = [];
@@ -21,46 +23,83 @@ app.use('/graphql', graphqlHTTP({
            price:Float!
            date:String!
         }
+        type User{
+            _id:ID!
+            email:String!
+            password:String
+        }
         input EventInput{
            title:String!
            description:String!
            price:Float!
            date:String!
         }
+        input UserInput{
+            email:String!
+            password:String!
+        }
         type RootQuery{
             events:[Event!]!
         }
         type RootMutation{
             createEvent(eventInput:EventInput):Event
+            createUser(userInput:UserInput):User
         }
         schema {
             query:RootQuery
             mutation:RootMutation
         }
     `),
-    
+
     rootValue: {
         events: () => {
-            return events;
+            return Event.find()
+                .then(events => {
+                    return events.map(event => {
+                        return { ...event._doc, _id: event.id }
+                    });
+                })
+                .catch(err => {
+                    throw err;
+                })
         },
         createEvent: (args) => {
-            const event=new Event({
+            const event = new Event({
                 title: args.eventInput.title,
                 description: args.eventInput.description,
                 price: +args.eventInput.price,
                 date: new Date(args.eventInput.date)
             });
             return event.save().then(
-                (result)=>{
+                (result) => {
                     console.log(result);
-                    return {...result._doc};
+                    return { ...result._doc, _id: result._doc._id.toString() };
                 }
 
-            ).catch(err=>{
+            ).catch(err => {
                 console.log(err);
                 throw err;
             });
-            return event;
+        },
+        createUser: (args) => {
+            bcrypt.hash(args.userInput.password, 12)
+            .then(hashedPassword=>{
+                const user=new User({
+                    email:args.userInput.email,
+                    password:hashedPassword
+                })
+            })
+            .then(result=>{
+                
+            })
+            .catch(err => {
+                    throw err;
+            });
+            const user = new User({
+                email: args.userInput.email,
+                password: args.userInput.password
+
+            })
         }
     },
     graphiql: true
